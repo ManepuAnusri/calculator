825d1af515f4f6af84e3d0bc1e43c8e8c8a20b0c
ManepuAnusri <71380028+ManepuAnusri@users.noreply.github.com>
2021-05-31 17:25:12 +0530

    calculator program

---
+++ Calculator/src/calculator/Calculatorapp.java
@@ -0,0 +1,34 @@
+package calculator;
+
+public class Calculatorapp {
+   String name;
+   
+   public int add(int x,int y) {
+	   return x+y;
+	   
+   }
+   public int subtract(int x,int y) {
+	   return x-y;
+	   
+   }
+	
+   public int multi(int x,int y) {
+	   return x*y;
+	   
+   }
+	
+   public int divide(int x,int y) {
+	   return x/y;
+	   
+   }
+   public int modulus(int x,int y) {
+	   return x%y;
+	   
+   }
+	
+
+	
+}
+	
+
+
---
+++ Calculator/src/calculator/mainclass.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class mainclass {
+	public static void main(String[] args) {
+		Calculatorapp c1=new Calculatorapp();
+		System.out.println("addition of 2 and 3 is :" + c1.add(2,3));
+		System.out.println("subtraction of 8 and 3 is :" + c1.subtract(8,3));
+		System.out.println("multiplication of 5 and 6 is :" + c1.multi(5,6));
+		System.out.println("division of 6 and 2 is :" + c1.divide(6,2));
+		System.out.println("modulus of 5 and 2  is :" + c1.modulus(5,2));
+		
+		
+		
+		
+	}
+
+}
